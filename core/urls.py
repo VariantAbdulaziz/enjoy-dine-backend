8e3069b56b82969f95633ce31f314733c3e948f4
Abdulaziz Ali <variant.abdulaziz@gmail.com>
2025-08-09 13:12:47 +0300

    Update schema view in URLs to disable authentication classes for public access

--- core/urls.py
+++ core/urls.py
@@ -68,6 +68,7 @@ schema_view = get_schema_view(
       license=openapi.License(name="BSD License"),
    ),
    public=True,
+   authentication_classes=[],
    permission_classes=(permissions.AllowAny,),
    patterns=api_only_patterns
 )
